20acb4205406cf1eb4b7b1cb2c1dde03dc9c7b9b
lmnghia911 <lmnghia911@gmail.com>
2022-05-19 22:23:16 +0700

    task gdsc 1

---
+++ script.js
@@ -0,0 +1,25 @@
+// This is for able to see chart. We are using Apex Chart. U can check the documentation of Apex Charts too..
+
+
+var chart = new ApexCharts(document.querySelector("#apex1"), options);
+chart.render();
+
+// Sidebar Toggle Codes;
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+var sidebarCloseIcon = document.getElementById("sidebarIcon");
+
+function toggleSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add("sidebar_responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove("sidebar_responsive");
+    sidebarOpen = false;
+  }
+}
